5a9759154fc83f93d5f8d57e0336d0848f5b7da5
chiragbokade33 <chiragbokade223@gmail.com>
2025-08-01 16:57:01 +0530

    best code ever

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'test.testhfiles.in',
+        hostname: 'api.hfiles.in',
         port: '',
         pathname: '/**',
       },
